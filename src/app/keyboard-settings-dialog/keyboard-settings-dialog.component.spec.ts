e546503404f9c15c2b6eb95799d2a906bb939cd3
Joel Gotthelf <jgotthelf20@gmail.com>
2024-01-03 00:22:35 -0500

    fix tests

--- src/app/keyboard-settings-dialog/keyboard-settings-dialog.component.spec.ts
+++ src/app/keyboard-settings-dialog/keyboard-settings-dialog.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeyboardSettingsDialogComponent } from './keyboard-settings-dialog.component';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 describe('KeyboardSettingsDialogComponent', () => {
   let component: KeyboardSettingsDialogComponent;
@@ -9,6 +15,11 @@ describe('KeyboardSettingsDialogComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [KeyboardSettingsDialogComponent],
+      imports: [MatDialogModule, MatSlideToggleModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     });
     fixture = TestBed.createComponent(KeyboardSettingsDialogComponent);
     component = fixture.componentInstance;
