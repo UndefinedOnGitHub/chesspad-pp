bc6d0083e7ebd9c6d863ff9ee5fcc79fbca1f188
Joel Gotthelf <jgotthelf20@gmail.com>
2024-01-07 13:22:48 -0500

    add tests

--- src/app/game-review.service.spec.ts
+++ src/app/game-review.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GameReviewService } from './game-review.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('GameReviewService', () => {
   let service: GameReviewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(GameReviewService);
   });
 
--- src/app/game-review/game-review.component.spec.ts
+++ src/app/game-review/game-review.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameReviewComponent } from './game-review.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { KeyboardComponent } from '../keyboard/keyboard.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { KeyboardButtonComponent } from '../keyboard-button/keyboard-button.component';
 
 describe('GameReviewComponent', () => {
   let component: GameReviewComponent;
@@ -8,7 +15,18 @@ describe('GameReviewComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [GameReviewComponent],
+      declarations: [
+        GameReviewComponent,
+        KeyboardComponent,
+        KeyboardButtonComponent,
+      ],
+      imports: [
+        HttpClientModule,
+        MatSelectModule,
+        MatCardModule,
+        MatDialogModule,
+        FontAwesomeModule,
+      ],
     });
     fixture = TestBed.createComponent(GameReviewComponent);
     component = fixture.componentInstance;
