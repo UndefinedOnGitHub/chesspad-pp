33767d746d8edbf92cae42473f31ab77b56494f3
Joel Gotthelf <jgotthelf20@gmail.com>
2024-03-01 23:09:23 -0500

    remove comment

--- src/app/finish-game-dialog/finish-game-dialog.component.ts
+++ src/app/finish-game-dialog/finish-game-dialog.component.ts
@@ -39,7 +39,6 @@ export class FinishGameDialogComponent {
     this.gameWinner = this.findGameWinner();
     this.onGameWinnerChange({ value: this.gameWinner });
     this.gameDisabled = data.disabled || false;
-    // debugger
   }
 
   findGameWinner(): '1-0' | '1/2-1/2' | '0-1' {
